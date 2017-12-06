b5799730b4c44d7f3cba816223cd003626b2f04e
Kevin Schaffter <kevinschaffter@gmail.com>
2017-12-05 20:29:34 -0500

    added text to homepage

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  # before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:edit, :update, :destroy]
 
   # GET /links
   # GET /links.json
@@ -66,9 +66,9 @@ class LinksController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_link
-    #   @link = Link.find(params[:id])
-    # end
+    def set_link
+      @link = Link.find(params[:id])
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,6 @@
 class Link < ApplicationRecord
+  validates :original, presence: true
+
+  validates :original, :format => URI::regexp(%w(http https))
+
 end
